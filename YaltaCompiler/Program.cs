ed58e6562082be288a202cc5cf05c417894e7df4
Andrii Hrytsenko <andrii.hrytsenko02@gmail.com>
2024-11-18 21:20:02 +0200

    matroshka style

---
+++ YaltaCompiler/Program.cs
@@ -0,0 +1,17 @@
+﻿using OurDartLangLexer.Lexer;
+using YaltaLangLexer.DataProviders;
+
+
+string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+string fileName = "comment_ignoring.yt";
+string filePath = Path.Combine(projectDirectory, "ExamplesOfCode", fileName);
+
+if (!File.Exists(filePath)) throw new Exception("File not found: " + filePath);
+
+var lexer = new Lexer();
+
+var sourceCodeArr = File.ReadAllText(filePath);
+
+lexer.ProcessInput(sourceCodeArr);
+
+OutputHandler.WriteToConsole(lexer.TokenTable, lexer.SymbolTable, lexer.ErrorTable);
--- YaltaLangParser/Parser.cs
+++ YaltaLangParser/Parser.cs
@@ -5,9 +5,10 @@ namespace YaltaLangParser;
 
 public class Parser
 {
-    public int NumLine = 1;
+    private int _numLine = 1;
     private readonly Lexer _lexer;
     private readonly int _lengthOfSymbolTable;
+    private
 
     public Parser(Lexer lexer)
     {
@@ -17,12 +18,64 @@ public class Parser
 
     public void ParseProgram()
     {
-        //TODO обробка таблиці від лексера
-        //TODO Спершу ми читаємо правило main -> {StatementList}
-        ParseToken("main", "keyword");
-        throw new NotImplementedException();
+        try
+        {
+            //TODO обробка таблиці від лексера
+            //TODO Спершу ми читаємо правило main -> StatementBlock
+            ParseToken("main", "keyword");
 
-        //TODO імплементація таблиці розбору
+            //TODO Потім ми читаємо правило StatementBlock -> { StatementList }
+            ParseStatementBlock();
+            Console.WriteLine("Parser: Синтаксичний аналіз завершено успішно");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Parser: Синтаксичний аналіз завершено з помилкою на рядку {_numLine}, {e.Message}");
+        }
+    }
+
+    private void ParseStatementBlock()
+    {
+        ParseToken("{", "symbol");
+        ParseStatementList();
+        ParseToken("}", "symbol");
+    }
+
+    private void ParseStatementList()
+    {
+        while (_lexer.TokenTable[_lengthOfSymbolTable].Lexeme != "}")
+        {
+            ParseStatement();
+        }
+    }
+
+    private void ParseStatement()
+    {
+        //TODO Statement -> PrintStatement
+        //                  | AssignmentStatement
+        //                  | IfStatement
+        //                  | WhileStatement
+        //                  | PrintStatement
+
+        switch (_lexer.TokenTable[].Lexeme)
+        {
+            case "print":
+                ParsePrintStatement();
+                break;
+            case "if":
+                ParseIfStatement();
+                break;
+            case "while":
+                ParseWhileStatement();
+                break;
+            default:
+                ParseAssignmentStatement();
+                break;
+        }
+        {
+
+        }
+
     }
 
     /// <summary>
@@ -33,6 +86,5 @@ public class Parser
     /// <param name="token"></param>
     private void ParseToken(string lexeme, string token)
     {
-        
     }
 }
